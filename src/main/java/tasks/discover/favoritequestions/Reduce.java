6f0b5a299c7d2a0996562bacfd0bb66be0e1c1b4
Marc Puig <mpuig@ac.upc.edu>
2018-10-11 11:38:05 +0200

    folder structure + tasks

---
+++ src/main/java/tasks/discover/favoritequestions/Reduce.java
@@ -0,0 +1,13 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) {
+        //TODO
+    }
+
+}
---
+++ src/main/java/tasks/discover/topquestions/Map.java
@@ -0,0 +1,18 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        //Tips: regex to remove all non-words and digits. [\\W|\\d]
+        //TODO
+    }
+}
