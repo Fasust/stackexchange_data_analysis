2523d913c507be9da7a19dd97ee2a86c20f4b5e1
Fasust <sebastianf333@googlemail.com>
2018-10-11 11:58:51 +0200

    Changed Input Format to XML in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import xml_reader.XmlInputFormat;
 
 public class Main {
 
@@ -20,16 +21,26 @@ public class Main {
             final Job job = Job.getInstance(new Configuration());
             job.setJarByClass(Main.class);
             job.setJobName("Word counter");
-            FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            //Input Format
+            job.setInputFormatClass(XmlInputFormat.class);
+
+            //Mapper
             job.setMapperClass(Map.class);
-            job.setReducerClass(Reduce.class);
             job.setMapOutputKeyClass(Text.class);
             job.setMapOutputValueClass(IntWritable.class);
+
+            //Reducer
+            job.setReducerClass(Reduce.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(IntWritable.class);
+
+            //Process Args
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
             System.exit(job.waitForCompletion(true) ? 0 : 1);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
