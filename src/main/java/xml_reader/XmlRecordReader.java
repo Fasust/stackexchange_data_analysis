e0e4644a58839b89b95b8958d0eab744f46e2727
Marc Puig <mpuig@ac.upc.edu>
2018-10-11 11:57:59 +0200

    main classes rename

--- src/main/java/xml_reader/XmlRecordReader.java
+++ src/main/java/xml_reader/XmlRecordReader.java
@@ -9,14 +9,6 @@ import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
