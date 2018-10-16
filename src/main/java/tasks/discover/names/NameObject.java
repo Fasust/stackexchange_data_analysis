ab058003a29528a9d112f01364f0c46c8047c670
kruspy <marckrupuig@gmail.com>
2018-10-16 16:01:00 +0200

    names task update

---
+++ src/main/java/tasks/discover/names/NameObject.java
@@ -0,0 +1,28 @@
+package tasks.discover.names;
+
+public class NameObject implements Comparable<NameObject> {
+
+    private Integer nameCount;
+
+    private String name;
+
+    public NameObject(Integer nameCount, String name) {
+        this.nameCount = nameCount;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(NameObject o) {
+        if(this.getNameCount().equals(o.getNameCount())) return 1;
+        if(this.getNameCount() > o.getNameCount()) return -1;
+        return 1;
+    }
+
+    public Integer getNameCount() {
+        return nameCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/tasks/discover/names/Reduce.java
+++ src/main/java/tasks/discover/names/Reduce.java
@@ -1,34 +1,31 @@
 package tasks.discover.names;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.TreeSet;
 
 
 public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-    private TreeMap<Integer, String> nameMap = new TreeMap<>();
+    private TreeSet<NameObject> nameMap = new TreeSet<>();
     private static final int OUTPUT_SIZE = 10;
 
     @Override
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) {
         int nameCount = 0;
         for(IntWritable value : values){
             nameCount++;
         }
 
         //Add to sorted map
-        nameMap.put(nameCount, key.toString());
+        nameMap.add(new NameObject(nameCount,key.toString()));
 
         //Make sure the map only consists of OUTPUT_SIZE amount of values (Highest)
         while (nameMap.size() > OUTPUT_SIZE){
-           nameMap.remove(nameMap.firstKey());
+           nameMap.pollFirst();
         }
     }
 
@@ -36,10 +33,10 @@ public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
     protected void cleanup(Context context) throws IOException, InterruptedException {
 
         //Output all Values in nameMap
-        for(Integer key : nameMap.keySet()) {
+        for(NameObject name : nameMap) {
             context.write(
-                    new Text(nameMap.get(key)),
-                    new IntWritable(key)
+                    new Text(name.getName()),
+                    new IntWritable(name.getNameCount())
             );
         }
 
