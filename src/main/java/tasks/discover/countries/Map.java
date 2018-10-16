041ece041dd5b77d9ed589412358765f8e7426b0
Fasust <sebastianf333@googlemail.com>
2018-10-16 13:29:46 +0200

    documentation

--- src/main/java/tasks/discover/countries/Map.java
+++ src/main/java/tasks/discover/countries/Map.java
@@ -20,8 +20,13 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 
             for (int i = 0; i < nList.getLength(); i++) {
 
+                /**
+                 * I would have liked to parse the location to limit it to just the "Country",
+                 * but there is no restriction on what users can type in the field location,
+                 * thusly there is no real schema to follow and thusly im just using in the un-parsed string.
+                 */
+                
                 String location = nList.item(i).getAttributes().getNamedItem("Location").getNodeValue();
-
                 context.write(new Text(location), new IntWritable(1));
 
             }
